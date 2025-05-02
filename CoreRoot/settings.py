e69b32ac9f9a94e999f88d7758c0d37f2758dbb8
Silvia-Wachira <wachirasilvia8.com>
2025-05-02 16:50:37 +0300

    install DRF

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'core_user.User'
 
 # Application definition
 
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'core.user',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
